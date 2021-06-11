ad7940cbbdca5389425554e0a8effbb27029665c
Parker Rex <parker.m.rex@gmail.com>
2021-06-11 12:07:03 -0400

    updated youtube link and header

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: 'https://www.youtube.com/channel/UCcuaQecz84wTuxKzr1Yxi4Q', title: 'YouTube' },
 ]
 
 export default headerNavLinks
