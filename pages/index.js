94517894346c6067dcb5ab127794e138168cd907
Parker Rex <parker.m.rex@gmail.com>
2021-07-27 10:25:15 -0400

    swapped convertkit for mailchimp

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { PageSeo } from '@/components/SEO'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
+import MailchimpSubscribe from 'react-mailchimp-subscribe'
 
 const MAX_DISPLAY = 5
 const postDateTemplate = { year: 'numeric', month: 'long', day: 'numeric' }
@@ -21,13 +22,13 @@ export default function Home({ posts }) {
         description={siteMetadata.description}
         url={siteMetadata.siteUrl}
       />
+      <h2 className="text-2xl font-bold leading-8 tracking-tight">Stay up to date</h2>
+      <MailchimpSubscribe
+        className="text-gray-900 dark: text-gray-100"
+        url={process.env.NEXT_PUBLIC_MAILCHIMP_URL}
+      />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-          Upgrade your brain
-          </h2>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">Get the best tech resources for unlocking your potential each week.</p>
-            <script async data-uid="ff5ecf66c9" src="https://awesome-creator-6998.ck.page/ff5ecf66c9/index.js"></script>
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
           </h1>
