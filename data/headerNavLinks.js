a81bd968fb4e4f9d519003e831c663950d93fb0a
Parker Rex <parker.m.rex@gmail.com>
2021-12-01 21:21:58 -0600

    less cringe

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
+  //  { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
   { href: 'https://www.youtube.com/channel/UCcuaQecz84wTuxKzr1Yxi4Q', title: 'YouTube' },
