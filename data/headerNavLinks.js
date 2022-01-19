1d00d664a30f302e1f77989fd0abbe82a2d67e6b
ParkerRex <parker.m.rex@gmail.com>
2022-01-19 16:09:18 -0600

    website updates

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -4,7 +4,7 @@ const headerNavLinks = [
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
   { href: 'https://www.youtube.com/channel/UCcuaQecz84wTuxKzr1Yxi4Q', title: 'YouTube' },
-  { href: 'https://pmbeginners.teachable.com/purchase?product_id=3273132', title: 'PM Course' },
+  { href: 'https://pmbeginners.teachable.com/', title: 'PM Course' },
 ]
 
 export default headerNavLinks
